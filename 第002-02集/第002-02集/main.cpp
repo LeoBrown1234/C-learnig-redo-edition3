7564f8dac21dfb39fd63255c16e9cd7801c91bb2
Leo Brown <448996350@qq.com>
2025-11-30 17:13:55 +0800

    建立连接，初始化项目

---
+++ 第002-02集/第002-02集/main.cpp
@@ -0,0 +1,16 @@
+/*
+第一个C程序，C书写helloworld
+2025-11-29 21:53
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! Hello C++!" << endl;
+	system("pause");
+	return 0;
+}
+
+
