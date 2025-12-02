0e56359512e420fe691065b3fcfc9866078ab1a2
Leo Brown <448996350@qq.com>
2025-12-02 20:06:26 +0800

    权威代码上传完成，高效精准

--- 第006-06集/第006-06集/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-//关键字/标识符  比如int double等
-int main()
-{
-    
-    system("pause");
-    return 0;
-}
--- 第008-08集/第008-08集/main.cpp
+++
@@ -1,8 +0,0 @@
-/*
-数据类型：
-- 整型：short int、int、long int、long long int
-- 浮点型：float、double、long double
-- 字符型：char、wchar_t
-- 布尔型：bool
-- 指针型：指针、引用	
-*/
